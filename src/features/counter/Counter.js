72420275a4138a6df0a97574bc3f2a1704834a8f
ashok <ashokkumar.a1999@gmail.com>
2024-02-13 17:22:47 +0530

    first commit

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './counter.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement,increment,incrementByAmount } from './counterSlice'
+
+function Counter() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch =useDispatch()
+  return (
+      <div className='flex flex-col gap-5 py-20'>
+          <div className=' counter text-white bg-red-600 text-center px-20 py-20 text-2xl font-bold '>{count}</div>
+          <div className='flex gap-5'>
+          <button className='border bg-slate-500 text-white text-sm px-5 py-5 rounded-xl' onClick={() => dispatch(decrement())}>decrement</button> 
+          <button className='border bg-slate-500 text-white text-sm px-5 py-5 rounded-xl' onClick={() => dispatch(increment())}>increment</button>
+        </div>
+          
+    </div>
+  )
+}
+
+export default Counter
